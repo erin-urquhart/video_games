f78d885680101ed8337978ff7433ad7290a02cbc
erin-urquhart <urquhart.erin@gmail.com>
2022-02-13 13:50:23 -0600

    Added controllers for tables, added code to show data from the tables on the pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  #get 'games/index'
+  #get 'games/show'
+  resources :games, only: [:index, :show]
+  resources :platforms, only: [:index, :show]
+  resources :genres, only: [:index, :show]
+  resources :employees, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
